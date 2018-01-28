077e0a64e05b94b171512d0f496d44500f37aa24
Eduardo Lacerda <eduardolacerdageo@gmail.com>
2018-01-28 00:23:28 -0200

    shrinked the code

--- code/randomForest/guanabaraS2_rf.js
+++ code/randomForest/guanabaraS2_rf.js
@@ -3,14 +3,7 @@ var rio_2016 = ee.Image('COPERNICUS/S2/20161110T130242_20161110T165117_T23KPQ');
 var rio_2016_ndvi = geet.ndviS2(rio_2016);
 var newfc = urbano.merge(floresta).merge(agua).merge(pasto);
 var cart_class = geet.rf(rio_2016_ndvi, newfc, 'cobertura', 10);
-
-var COLOR = {
-  WATER: '0066ff',
-  FOREST: '009933',
-  PASTURE: '99cc00',
-  URBAN: 'ff0000'
-};
-
+var COLOR = {WATER: '0066ff', FOREST: '009933', PASTURE: '99cc00', URBAN: 'ff0000'};
 Map.addLayer(cart_class, { min: 0, max: 3, palette: [COLOR.URBAN, COLOR.FOREST, COLOR.WATER, COLOR.PASTURE] }, 'class');
 
 
